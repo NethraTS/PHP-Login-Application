805fc4c00cbd5f914f084794415fc724344fd976
Nethra <nethra.suthagar@terralogic.com>
2017-03-30 16:01:07 +0530

    Adding Hidden files and sample php file

---
+++ sites/all/modules/sample.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  HumanClass class file
+ *   
+ *  Php version 5.2
+ *
+ *  @category Human_Family_A
+ *  @package  Human_Family_A
+ *  @author   purencool <purencool@gmail.com>
+ *  @license  http://opensource.org/licenses/GPL-3.0 GNU Public License
+ *  @link     http://purencool.com
+ */
+?>
+
